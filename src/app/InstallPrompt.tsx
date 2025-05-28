b960276fcdda3b9838fcc07ac1c888d4fef9b4f2
Jellan Arta <jellanarta@gmail.com>
2025-05-28 14:23:41 +0800

    ubah bg install

--- src/app/InstallPrompt.tsx
+++ src/app/InstallPrompt.tsx
@@ -28,7 +28,7 @@ export function InstallPrompt() {
   return (
     <div className='bg-gray-600 text-gray-50'>
       <p>Install aplikasi ini ke perangkat Anda!</p>
-      <button onClick={install}>Install</button>
+      <button className='bg-blue text-white p-3' onClick={install}>Install Di Sini</button>
     </div>
   )
 }
